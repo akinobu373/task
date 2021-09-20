c993e5ff76802b2cdd5532cd6b64520687b933e4
akinobu <siguiente.football.373@gmail.com>
2021-09-20 21:20:52 +0900

    タスク管理アプリ課題完了

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            [
+                'title' => 'テストタスク１',
+                'body' => 'テストタスクの内容です。'
+            ]
+        ];
+
+        DB::table('tasks')->insert($param);
+    }
+}
---
+++ resources/views/task/edit.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>edit</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+
+    <h1>タスク編集</h1>
+    @if ($errors->any())
+    <div class="error">
+        <p>
+            <b>[エラー内容]</b>
+        </p>
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <form action="/tasks/{{ $task->id }}" method="post">
+        @csrf
+        @method('PATCH')
+        <p>
+            <label for="title">[タイトル]</label>
+            <input type="text" name="title" value="{{ old('title',$task->title )}}">
+        </p>
+        <p>
+            <label for="body">[本文]</label>
+            <textarea name="body" cols="30" rows="10" required>{{ old('body', $task->body) }}</textarea>
+        </p>
+        <div class="button-group">
+            <input type="submit" value="更新">
+            <button onclick="location.href='/tasks'">詳細に戻る</button>
+        </div>
+    </form>
+</body>
+
+</html>
